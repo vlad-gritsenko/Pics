b5a5d72554867eacdeccb632dce423de78f9721c
Vlad Gritsenko <vladxmas@gmail.com>
2017-10-18 22:41:12 +0300

    minor changes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,11 +4,11 @@ app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
     .when('/', {
       templateUrl: 'view/directory.html',
-      controller: 'getPhones'
+      controller: 'getPhotos'
     })
     .when('/album/:album_id', {
       templateUrl: 'view/album.html',
-      controller: 'getAlbums'
+      controller: 'getAlbum'
     }).otherwise({
       redirectTo:'/'
     });
@@ -18,14 +18,13 @@ app.config(['$routeProvider', function($routeProvider) {
 var photosUrl = 'http://jsonplaceholder.typicode.com/photos';
 var albumUrl = 'http://jsonplaceholder.typicode.com/albums/:album_id/photos';
 
-app.controller('getPhones', function ($scope, $http) {
+app.controller('getPhotos', function ($scope, $http) {
 
   $scope.limit = 10;
 
   $http.get(photosUrl).then(function(response) {
     $scope.photos = response.data;
   });
-
 
   window.onscroll = function(ev) {
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
@@ -38,9 +37,9 @@ app.controller('getPhones', function ($scope, $http) {
   };
 });
 
-app.controller('getAlbums', function ($scope, $http, $routeParams) {
-  $http.get(albumUrl).then(function(response) {
-    $scope.albums = response.data;
-    console.log($scope.albums);
-  });
-});
+app.controller('getAlbum', ['$scope', '$location', '$interpolate', function ($scope, $location, $interpolate) {
+  $scope.test = function() {
+    var url = $interpolate('/album/{{album_id}}')
+    $location.path(url)
+  }
+}]);
